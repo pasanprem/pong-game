0a3e253624ae3fb2f4b879fe0536ff710f42996b
pasanprem <46164432+pasanprem@users.noreply.github.com>
2023-02-07 14:45:05 +0000

    Paddle done

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
 # Pong Game - Part of the 100 Days of Code Udemy course
 
 from turtle import Turtle, Screen
-
-# from paddle import Paddle
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.update()
 
 
-paddle = Turtle()
-paddle.color("white")
-paddle.shape("square")
-paddle.penup()
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.goto(x=350, y=0)
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
 
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
+game_is_on = True
 
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
