a86cf980e4835e7e22ac7626b35e4a424057fa75
pasanprem <46164432+pasanprem@users.noreply.github.com>
2023-02-07 16:09:29 +0000

    Bouncing implemented

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -11,11 +13,10 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+
 
 screen.update()
-
-
-
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -26,6 +27,17 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # Ball needs to bounce
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
